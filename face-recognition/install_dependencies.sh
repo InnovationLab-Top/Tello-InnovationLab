ef70ed0c31b1ee45e90d9ed7c2f78be262106294
giorgiozannini <44499913+giorgiozannini@users.noreply.github.com>
2022-05-23 12:16:29 +0200

    added mttkinter python3-tk

--- face-recognition/install_dependencies.sh
+++ face-recognition/install_dependencies.sh
@@ -3,8 +3,8 @@ activate () {
     . $PWD/venv/bin/activate
 }
 activate
-pip install cmake face_recognition opencv-python numpy
-sudo apt install -y libswscale-dev libavcodec-dev libavutil-dev libboost-all-dev 
+pip install cmake face_recognition opencv-python numpy mttkinter
+sudo apt install -y libswscale-dev libavcodec-dev libavutil-dev libboost-all-dev python3-tk
 sudo apt install -y python-imaging-tk
 git clone https://github.com/DaWelter/h264decoder.git
 cd h264decoder
