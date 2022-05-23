3eaddd46c404c8d8a967d344cf26042e71e057aa
AlessandroZaccari <34105512+AlessandroZaccari@users.noreply.github.com>
2022-05-23 17:43:51 +0200

    Delete block_autolanding.py

--- speech-recognition/block_autolanding.py
+++
@@ -1,8 +0,0 @@
-from tello import Tello
-import time
-
-def block_autolanding(Tello_instance):
-    while True:
-        Tello_instance.send_command("Block autolanding")
-        print("Auto-landing blocked")
-        time.sleep(5)
