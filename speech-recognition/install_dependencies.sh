7a3c1a321760a37e0575c5e246cf68f7552a89ed
giorgiozannini <44499913+giorgiozannini@users.noreply.github.com>
2022-05-23 11:17:17 +0200

    added cmake

--- speech-recognition/install_dependencies.sh
+++ speech-recognition/install_dependencies.sh
@@ -7,6 +7,7 @@ pip install deeppavlov
 python -m deeppavlov install asr_tts
 python -m deeppavlov download asr_tts
 pip install python-levenshtein sounddevice torchaudio
+pip install cmake
 git clone https://github.com/DaWelter/h264decoder
 cd h264decoder/
 sudo apt install libswscale-dev libavcodec-dev libavutil-dev
